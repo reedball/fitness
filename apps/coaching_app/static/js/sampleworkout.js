28e244e8eb2c83272a32b5f98b5c95d0a3a2238a
Aida Pirahmadi <Aidapirahmadi@yahoo.com>
2020-01-09 09:53:59 -0800

    youtube

--- apps/coaching_app/static/js/sampleworkout.js
+++ apps/coaching_app/static/js/sampleworkout.js
@@ -1,21 +1,6 @@
 $(document).ready(function () {
     $("#samplemm").click(function () {
         const $video = $("#video")
-        $video.append(`<iframe width="779" height="438" src="https://www.youtube.com/watch?v=fzBgSITMnGo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-
-
-        // displaymm()
-        // $.get("https://www.youtube.com/embed/qJDpMyUbunw", displaymm)
-        // console.log("hello")
-        // function displaymm(data) {
-        //     console.log("function working")
-        //     console.log(data)
-        //     showMM(data);
-        // }
-        // function showMM(data) {
-        //     console.log(data)
-        //     const $video = $("#video")
-        //     $video.append(`<video src="data"></div>`)
-        // }
+        $video.append(`<iframe width="710" height="397" src="https://www.youtube.com/embed/fzBgSITMnGo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
     })
 });
